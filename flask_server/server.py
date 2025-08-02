09957e949acc1ec7c0eaebd9958a079a5fdf6d77
Hasan Alfaraj <hasan.a.alfaraj@gmail.com>
2025-08-03 00:58:10 +0300

    Deleted client

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,14 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory, jsonify
 
-# commentpip install pipreqs
+# Initialize Flask
 app = Flask(__name__)
 
 
-# Members API Route
+@app.route("/")
+def welcome():
+    return "Hello world!"
+
+
+# API endpoint
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return jsonify({"members": ["Member1", "Member2", "Member3"]})
 
 
+# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
