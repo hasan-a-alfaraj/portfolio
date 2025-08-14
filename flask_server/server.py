2b66b8bf24025e563973014e1ab31279fc26c595
Hasan Alfaraj <hasan.a.alfaraj@gmail.com>
2025-08-14 11:57:56 +0300

    Added portfolio index.html

--- flask_server/server.py
+++ flask_server/server.py
@@ -10,31 +10,13 @@ import matplotlib.pyplot as plt
 app = Flask(__name__, template_folder="templates")
 
 
-def get_meme():
-    url = "https://meme-api.com/gimme"
-    response = json.loads(requests.request("GET", url).text)
-    meme_large = response["preview"][-2]
-    # subreddit = response["subreddit"]
-    return meme_large
-
-
-def get_data():
-    url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/03-population-by-nationality-and-sex/records?limit=20"
-    response = json.loads(requests.request("GET", url).text)
-    return response
 
 
 @app.route("/")
-def welcome():
-    meme_pic = get_meme()
-    return render_template("index.html", meme_pic=meme_pic)
-
-
-@app.route("/data")
-def data():
-    return get_data()
+def index():
+    return render_template("index.html")
 
 
 # Run the app
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
